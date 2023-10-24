595cbbb2a70f3b2c2aaee2c85c66a9b960299af3
bezoj <jernej.bezjak9@gmail.com>
2023-10-24 16:26:01 +0200

    Remake of TO DO app

--- index.js
+++ index.js
@@ -1,83 +1,106 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const inputEl = document.getElementById("input-el");
-    const searchBtnEl = document.getElementById("search-btn");
-    const divLiEl = document.getElementById("list-el");
-    const trashIconEl = document.getElementById("trash-icon")
-    const clearBtnEl = document.getElementById("clear-btn")
-    const radioBtnEl = document.getElementById("radio-btn")
-    const clearTaskEl = document.getElementById("clear-task")
-    let taskArray = [];
-    let taskCounter = 0
+document.addEventListener("DOMContentLoaded", function(){
 
-    function updateList() {
+    
+const addBtn = document.getElementById("search-btn")
+const checkBox = document.getElementById("check-box")
+const inputEl = document.getElementById("input-el")
+const listEl = document.getElementById("list-el")
+const clearBtn = document.getElementById("clear-btn")
+const pendingTask = document.getElementById("clear-task")
+const completedTask = document.getElementById("completed-tasks")
+const completedH6 = document.getElementById("h6-completed")
 
-        divLiEl.innerHTML = "";
-        taskArray.forEach((task, index) => {
-            const listItem = document.createElement("div");
-            listItem.classList.add("div--li");
-            listItem.innerHTML = `
-                <li>
-                    <input type="radio" class="radio--button" id="radio-btn">${task}
-                </li>
-                <button class="remove--btn" data-index="${index}"><i class="fa-solid fa-trash" id="trash-icon"></i></button>
-            `;
+let countTask = 0
 
-            divLiEl.appendChild(listItem);
-        });
+pendingTask.innerHTML = `You have ${countTask} pending Tasks.`
+
+function saveData() {
+    localStorage.setItem("data", listEl.innerHTML)
+}
+
+
+addBtn.addEventListener("click", function () {
+   
+     if (inputEl === ''){
+        alert("You need to write something") 
+    } else {
+        let li = document.createElement("li")
+        let span = document.createElement("span")
+        span.innerHTML = "X"
+        li.innerHTML = inputEl.value
+        listEl.appendChild(li)
+        li.appendChild(span)
+        countTask++
+        pendingTask.innerHTML = `You have ${countTask} pending Tasks.`
     }
+    inputEl.value = ""
+    saveData()
+})
 
-    function addTasks() {
-        searchBtnEl.addEventListener("click", function () {
-            const taskText = inputEl.value.trim();
-            if (taskText !== "") {
-                taskArray.push(taskText);
-                inputEl.value = "";
-                console.log(taskArray)
-                taskCounter++
-                pendingTasks()
-                updateList();
-            }
-        });
-    }
 
-    divLiEl.addEventListener("click", function (event) {
-        if (event.target.classList.contains("remove--btn")) {
-            const indexToRemove = parseInt(event.target.getAttribute("data-index"));
-            if (!isNaN(indexToRemove) && indexToRemove >= 0 && indexToRemove < taskArray.length) {
-                taskArray.splice(indexToRemove, 1);
-                taskCounter--
-                pendingTasks()
-                updateList();
+listEl.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+        if (e.target.classList.contains("checked")) {
+            completedTask.appendChild(e.target)
+            if (completedTask.querySelectorAll("li.checked").length > 0){
+                completedH6.textContent = "Completed"
             }
         }
-    });
-    
-
-    clearBtnEl.addEventListener("click", function () {
-        const taskReset = 0
-        taskCounter = taskReset
-        taskArray = []
-        clearTaskEl.innerHTML = `
-            <p> You have ${taskReset} pending tasks </p>
-        `
-        updateList();
-    })
-
-
-    function pendingTasks() {
-        clearTaskEl.innerHTML = `
-            <p> You have ${taskCounter} pending tasks </p>
-        `
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove()
+        saveData()
+        countTask--
+        pendingTask.innerHTML = `You have ${countTask} pending Tasks.`
     }
+}, false)
 
-    /*
-    radioBtnEl.addEventListener("click", function() {
-        
+
+
+clearBtn.addEventListener("click", function(e) {
+    const listItems = listEl.querySelectorAll("li")
+    const completedItems = completedTask.querySelectorAll("li")
+    listItems.forEach(function(e) {
+        e.remove()
+        countTask = 0
+        pendingTask.innerHTML = `You have ${countTask} pending Tasks.`
     })
-    */
+    completedItems.forEach(function(e) {
+        e.remove()
+        countTask = 0
+        pendingTask.innerHTML = `You have ${countTask} pending Tasks.`
+    })
+    if (completedTask.querySelectorAll("li.checked").length === 0) {
+        completedH6.innerHTML = ""
+    }
+})
 
-    pendingTasks()
-    addTasks();
-    updateList();
-    
-});
+
+
+completedTask.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked")
+        if (!e.target.classList.contains("checked")) {
+            listEl.appendChild(e.target)
+            if (completedTask.querySelectorAll("li.checked").length === 0) {
+                completedH6.innerHTML = ""
+            }
+        }
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove()
+        countTask--
+        pendingTask.innerHTML = `You have ${countTask} pending Tasks.`
+            if (completedTask.querySelectorAll("li.checked").length === 0) {
+            completedH6.innerHTML = ""
+            }
+    }
+})
+
+
+
+
+
+
+
+})
